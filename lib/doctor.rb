e65b7e3362a150730a113ef4d0d4183b7f73e284
Philippe Timothe <philippetimothe@gmail.com>
2020-05-16 14:06:07 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -2,9 +2,16 @@ require 'pry'
 class Doctor 
   attr_accessor :name 
   
+  @@all = []
+  
   def initialize(name)
     @name = name 
+    @@all << self
   end
+  
+  def self.all
+    @@all
+  end 
   
   
   
